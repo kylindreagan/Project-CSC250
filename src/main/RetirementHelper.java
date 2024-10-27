308f6d8ba75b220c4c23708fefb20e9b81b4451d
kylindreagan <155770203+kylindreagan@users.noreply.github.com>
2024-10-27 19:23:10 -0400

    I don't know what I am doing

--- src/main/RetirementHelper.java
+++ src/main/RetirementHelper.java
@@ -29,15 +29,14 @@ public class RetirementHelper {
 
     public static List<Integer> Total_Required_Retirement_Income(float FIN, int LE, int RA, int CA, float OIAR, float Inflate, float Invest){
         List<Integer> TRRI = new ArrayList<>();
-        float Yearly_Needs = FIN - OIAR * 12; //OIAR = Other Income After Retirement 
+        float Yearly_Needs = (FIN - OIAR*12); //OIAR = Other Income After Retirement 
         System.out.println(Yearly_Needs);
         float Total_Needs = 0.0f;
         TRRI.add(0);
         for (int i = LE; i >= RA; i--) {
-            Total_Needs += (Yearly_Needs * Math.pow(1+Inflate,i-(CA-1)));
-            Total_Needs *= (1-Invest);
+           Total_Needs += (Yearly_Needs * (float)Math.pow(1+Inflate,i-CA)) / (float)Math.pow(1 + Invest, i - RA + 1);
             TRRI.add(Math.round(Total_Needs));
-            System.out.println(Total_Needs);
+            System.out.println("Year " + i + " Total Needs (discounted): " + Total_Needs);
         }
         Collections.reverse(TRRI); // Reverse the list in-place for correct order
         return TRRI;
