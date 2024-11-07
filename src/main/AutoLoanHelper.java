b75468debf0e994a0d122a48e27b053f52ed2d40
Gianna-Mantegna <gianna.mantegna@my.maryvillecollege.edu>
2024-11-07 12:13:57 -0500

    Auto Loan Updates
    Added Functionality for radio buttons, Clear, and Reset buttons. Calculate Button and errors in progress

---
+++ src/main/AutoLoanHelper.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author giann
+ */
+public class AutoLoanHelper {
+    public static String carPriceCalculate (int lTerm, float intRate, float sTax, float cIncent, float dPay, float trade, float fees, float monthly){
+        
+        return "";
+    }
+    
+    public static String monthlyPaymentCalculate(int lTerm, float intRate, float sTax, float cIncent, float dPay, float trade, float fees, float car){
+        
+        return "";
+    }
+}
